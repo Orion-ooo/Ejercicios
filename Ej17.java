05dcb7be879d20925fb37ab3e0de26b45310f8e5
Daniel Figueroa <daniel.figueroamanjarres@unicolombo.edu.co>
2024-02-29 14:19:57 -0500

    Ejercicio 17

---
+++ Ej17.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ej17 {
+    public static void main(String[] abc){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el primer número: ");
+        double n1 = entrada.nextDouble();
+
+        System.out.println("Ingrese el segundo número: ");
+        double n2 = entrada.nextDouble();
+
+        if(n1 > n2){
+            System.out.println("El número mayor es: " + n1);
+        }else{
+            System.out.println("El número mayor es: " + n2);
+        }
+    }
+    
+}
