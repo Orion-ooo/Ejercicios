b92dab187c910c36d46e8e45d2080f9ab5f4a79e
Daniel Figueroa <daniel.figueroamanjarres@unicolombo.edu.co>
2024-03-20 16:08:05 -0500

    Ejercicio 38

---
+++ Ej38.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ej38 {
+    public static void main(String[] abc){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese su edad: ");
+        int edad = entrada.nextInt();
+        int manzana = 4;
+
+        double gramosConsumidos = edad * 8000;
+        double cantManzanas = gramosConsumidos / 4;
+
+        System.out.println("Usted a comsumido " + gramosConsumidos + " gramos de manzana en su vida");
+        System.out.println("Es decir, " + cantManzanas + " manzanas");
+
+        entrada.close();
+    }
+}
