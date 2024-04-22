ef951def7e76e6ebd5e85250bd2bda99aca4f5b6
Daniel Figueroa <daniel.figueroamanjarres@unicolombo.edu.co>
2024-04-21 20:58:21 -0500

    Ejercicio 166

---
+++ Ej166.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ej166 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese su peso en kg ");
+        double peso = entrada.nextDouble();
+        
+        System.out.println("Ingrese su estatura en metros: ");
+        double estatura = entrada.nextDouble();
+        
+        double imc = peso / (estatura * estatura);
+        
+        System.out.println("Su IMC es " + imc);
+        entrada.close();
+    }
+}
